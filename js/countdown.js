f388de48e64d39bc130d34181b75fde3b43e3538
TeraB12 <mateovpereyra@gmail.com>
2022-06-07 12:34:08 -0300

    FallingLife V1.2 Reloj

---
+++ js/countdown.js
@@ -0,0 +1,30 @@
+simplyCountdown('#cuenta', {
+	year: 2022, // required
+	month: 6, // required
+	day: 13, // required
+	hours: 23, // Default is 0 [0-23] integer
+	minutes: 59, // Default is 0 [0-59] integer
+	seconds: 0, // Default is 0 [0-59] integer
+	words: { //words displayed into the countdown
+		days: { singular: 'Día', plural: 'Días' },
+		hours: { singular: 'Hora', plural: 'Horas' },
+		minutes: { singular: 'Minuto', plural: 'Minutos' },
+		seconds: { singular: 'Segundo', plural: 'Segundos' }
+	},
+	plural: true, //use plurals
+	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+	// in case of inline set to false
+	enableUtc: false, //Use UTC as default
+	onEnd: function() {
+		alert('El evento ya comenzó');
+		return; 
+	}, //Callback on countdown end, put your own function here
+	refresh: 1000, // default refresh every 1s
+	sectionClass: 'simply-section', //section css class
+	amountClass: 'simply-amount', // amount css class
+	wordClass: 'simply-word', // word css class
+	zeroPad: false,
+	countUp: false
+});
+
